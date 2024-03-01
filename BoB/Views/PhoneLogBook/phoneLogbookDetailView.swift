5cf0145dc7bb0972e1f2ab772f59292eb956f689
Kim Martini <dr.kim.martini@gmail.com>
2024-03-01 10:12:02 -0800

    Moved the detail row to within phoneLogBookDetailView

--- BoB/Views/PhoneLogBook/phoneLogbookDetailView.swift
+++ BoB/Views/PhoneLogBook/phoneLogbookDetailView.swift
@@ -12,21 +12,21 @@ struct phoneLogbookDetailView: View {
         NavigationStack {
                 List {
                     Section("Sample Details"){
-                        phoneLogbookDetailRow(header: "Minimum Water Temperature", content: "7.0 °C")
-                        phoneLogbookDetailRow(header: "Maximum Underwater Depth", content: "44.0 m")
-                        phoneLogbookDetailRow(header: "Start Time", content: "Oct 17, 2023 at 12:10:39 PM PST")
-                        phoneLogbookDetailRow(header: "End Time", content: "Oct 17, 2023 at 12:21:22 PM")
-                        phoneLogbookDetailRow(header: "Samples", content: "1430")
-                        phoneLogbookDetailRow(header: "Sampling Frequency", content: "10 Hz")
-                        phoneLogbookDetailRow(header: "Source", content: "Kim's Apple Watch")
+                        phoneDetailRow(header: "Minimum Water Temperature", content: "7.0 °C")
+                        phoneDetailRow(header: "Maximum Underwater Depth", content: "44.0 m")
+                        phoneDetailRow(header: "Start Time", content: "Oct 17, 2023 at 12:10:39 PM PST")
+                        phoneDetailRow(header: "End Time", content: "Oct 17, 2023 at 12:21:22 PM")
+                        phoneDetailRow(header: "Samples", content: "1430")
+                        phoneDetailRow(header: "Sampling Frequency", content: "10 Hz")
+                        phoneDetailRow(header: "Source", content: "Kim's Apple Watch")
                     }
                     
                     Section("Device Details"){
-                        phoneLogbookDetailRow(header: "Name", content: "Apple Watch")
-                        phoneLogbookDetailRow(header: "Manufacturer", content: "Apple Inc.")
-                        phoneLogbookDetailRow(header: "Model", content: "Ultra")
-                        phoneLogbookDetailRow(header: "Hardware Version", content: "Watch6,18")
-                        phoneLogbookDetailRow(header: "Software Version", content: "10.3")
+                        phoneDetailRow(header: "Name", content: "Apple Watch")
+                        phoneDetailRow(header: "Manufacturer", content: "Apple Inc.")
+                        phoneDetailRow(header: "Model", content: "Ultra")
+                        phoneDetailRow(header: "Hardware Version", content: "Watch6,18")
+                        phoneDetailRow(header: "Software Version", content: "10.3")
                     }
                 }
                 .navigationBarTitleDisplayMode(.inline)
@@ -39,6 +39,22 @@ struct phoneLogbookDetailView: View {
         }
     }
 
+}
+
+
+struct phoneDetailRow: View {
+    let header: String
+    let content: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(header)
+                .font(.callout)
+                .foregroundColor(Color.gray)
+            Text(content)
+                .font(.body)
+        }
+    }
 }
 
 #Preview {
