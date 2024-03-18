1be671fc01f9fc9954053c9833d9d2bf88666ab8
Kim Martini <dr.kim.martini@gmail.com>
2024-03-18 12:00:01 -0700

    Commented out code that saves data directly to CoreData. Just hardcoded some values for testing

--- watchBoB Watch App/Models/MotionDataModel/MetadataLogger.swift
+++ watchBoB Watch App/Models/MotionDataModel/MetadataLogger.swift
@@ -9,49 +9,69 @@ import Foundation
 import CoreData
 import WatchKit
 
-public class SessionMetadata: NSManagedObject {
-    @NSManaged public var sessionID: UUID
-    @NSManaged public var startTime: Date
-    @NSManaged public var endTime: Date
-    @NSManaged public var stopTime: Date?
-    @NSManaged public var startCoordinates: Double
-    @NSManaged public var stopCoordinates: Double
-    @NSManaged public var startLatitude: Double
-    @NSManaged public var startLongitude: Double
-    @NSManaged public var stopLatitude: Double
-    @NSManaged public var stopLongitude: Double
-    @NSManaged public var samplingFrequency: Int16
-    @NSManaged public var stopCoordinatesLatitude: Double
-    @NSManaged public var stopCoordinatesLongitude: Double
-    
-    var isStopped = false
-    
-    func stop() {
-        guard !isStopped else { return } 
-           isStopped = true
-           endTime = Date()
-      }
-}
+//public class SessionMetadata: NSManagedObject {
+//    @NSManaged public var sessionID: UUID
+//    @NSManaged public var startTime: Date
+//    @NSManaged public var endTime: Date
+//    @NSManaged public var stopTime: Date?
+//    @NSManaged public var startCoordinates: Double
+//    @NSManaged public var stopCoordinates: Double
+//    @NSManaged public var startLatitude: Double
+//    @NSManaged public var startLongitude: Double
+//    @NSManaged public var stopLatitude: Double
+//    @NSManaged public var stopLongitude: Double
+//    @NSManaged public var samplingFrequency: Int16
+//    @NSManaged public var stopCoordinatesLatitude: Double
+//    @NSManaged public var stopCoordinatesLongitude: Double
+//    
+//    var isStopped = false
+//    
+//    func stop() {
+//        guard !isStopped else { return } 
+//           isStopped = true
+//           endTime = Date()
+//      }
+//}
 
 class MetadataLogger: NSObject, ObservableObject, CLLocationManagerDelegate {
-    let context = PersistenceController.shared.container.viewContext
-    var sessionMetadata: SessionMetadata?
+    // Temporarily remove logging directly to CoreData
+//    let context = PersistenceController.shared.container.viewContext
+//    var sessionMetadata: SessionMetadata?
+    
+    // define an ID for the metadata
+    var sessionID: UUID?
+    // define the start data
+    var startDatetime: Date?
+    var startLatitude: Double = 0.0
+    var startLongitude: Double = 0.0
+    // define the stop data
+    var stopDatetime: Date?
+    var stopLatitude: Double = 0.0
+    var stopLongitude: Double = 0.0
+    
     var isLogging = false
     let locationManager = CLLocationManager()
     var currentLocation: CLLocationCoordinate2D?
 
     func startLogging() {
-        sessionMetadata = SessionMetadata(context: context)
-        sessionMetadata?.sessionID = UUID()
-        sessionMetadata?.startTime = Date()
-
-        sessionMetadata?.samplingFrequency = 10
-
-        do {
-            try context.save()
-        } catch {
-            print("Failed to save session metadata: \(error)")
-        }
+        // Temporarily remove logging directly to CoreData
+//        sessionMetadata = SessionMetadata(context: context)
+//        sessionMetadata?.sessionID = UUID()
+//        sessionMetadata?.startTime = Date()
+//
+//        sessionMetadata?.samplingFrequency = 10
+//
+//        do {
+//            try context.save()
+//        } catch {
+//            print("Failed to save session metadata: \(error)")
+//        }
+        
+        // replace with just updating the local variables
+        sessionID = UUID()
+        startDatetime = Date()
+        startLatitude = 99.0
+        startLongitude = 99.0
         
         locationManager.startUpdatingLocation()
 
@@ -59,30 +79,37 @@ class MetadataLogger: NSObject, ObservableObject, CLLocationManagerDelegate {
         
         isLogging = true
         
-        self.sessionMetadata = nil
+//        self.sessionMetadata = nil
     }
 
     func stopLogging(stopCoordinates: CLLocationCoordinate2D) {
-        guard let sessionMetadata = sessionMetadata else {
-                print("Session metadata not found")
-                return
-            }
-        sessionMetadata.stopTime = Date()
-        sessionMetadata.stopCoordinatesLatitude = stopCoordinates.latitude
-        sessionMetadata.stopCoordinatesLongitude = stopCoordinates.longitude
-
-        do {
-            try context.save()
-            print("Session metadata saved")
-        } catch {
-            print("Failed to save session metadata: \(error.localizedDescription)")
-        }
+        // Temporarily remove logging directly to CoreData
+//        guard let sessionMetadata = sessionMetadata else {
+//                print("Session metadata not found")
+//                return
+//            }
+//        sessionMetadata.stopTime = Date()
+//        sessionMetadata.stopCoordinatesLatitude = stopCoordinates.latitude
+//        sessionMetadata.stopCoordinatesLongitude = stopCoordinates.longitude
+//
+//        do {
+//            try context.save()
+//            print("Session metadata saved")
+//        } catch {
+//            print("Failed to save session metadata: \(error.localizedDescription)")
+//        }
+        
+        // replace with just updating the local variables
+        sessionID = UUID()
+        stopDatetime = Date()
+        stopLatitude = -99.0
+        stopLongitude = -99.0
         
         locationManager.stopUpdatingLocation()
 
         isLogging = false
         
-        self.sessionMetadata = nil
+//        self.sessionMetadata = nil
     }
     
     // MARK: - CLLocationManagerDelegate
