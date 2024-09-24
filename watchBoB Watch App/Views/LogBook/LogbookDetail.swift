050d6903c4a6fb0c706473734434379248eccb9d
Kim Martini <dr.kim.martini@gmail.com>
2024-09-24 14:37:36 -0700

    Remove unused code

--- watchBoB Watch App/Views/LogBook/LogbookDetail.swift
+++ watchBoB Watch App/Views/LogBook/LogbookDetail.swift
@@ -9,41 +9,9 @@ import SwiftUI
 
 struct LogbookDetail: View {
     let record: SampleSet
-    
-    // Create the watch session manager
-    @StateObject private var watchSession = WatchSessionManager()
-    
-    // make a dynamic message
-    @State private var watchMessage: String = ""
-    // make a dynamic dictionary
-    @State private var watchDictionary: [String: Any] = [:]
-    
+
     // time stamp formatter
     let timeStampFormatter = TimeStampManager()
-    
-    private var parsedData: [[String: Any]]? {
-        return parseJSON()
-    }
-    
-    private func parseJSON() -> [[String: Any]]? {
-        guard let jsonString = record.gpsJSON else {
-            print("No JSON string found")
-            return nil
-        }
-        print("JSON String: \(jsonString)") // Debugging line
-        let data = Data(jsonString.utf8)
-        do {
-            if let jsonArray = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] {
-                return jsonArray
-            } else {
-                print("JSON is not an array of dictionaries")
-                return nil
-            }
-        } catch {
-            print("Error parsing JSON: \(error)")
-            return nil
-        }
-    }
     
     var body: some View {
         ScrollView {
@@ -56,18 +24,6 @@ struct LogbookDetail: View {
                 DetailRow(header: "End Coordinates", content: String(format: "%0.3f", record.stopLatitude)+" N,"+String(format: "%0.3f", record.stopLongitude)+" E")
                 DetailRow(header: "Samples", content: "\(record.getMotionDataCount())")
                 DetailRow(header: "Sampling Frequency", content: "10 Hz")
-//                if let dataArray = parsedData {
-//                    ForEach(dataArray.indices, id: \.self) { index in
-//                        let item = dataArray[index]
-//                        if let timestamp = item["timestamp"] as? String,
-//                           let latitude = item["latitude"] as? Double,
-//                           let longitude = item["longitude"] as? Double {
-//                            DetailRow(header: "Location Sample \(index + 1)", content: "Time: \(timeStampFormatter.formattedTime(from: timestamp)), \nLat: \(latitude), \nLon: \(longitude)")
-//                        }
-//                    }
-//                } else {
-//                    DetailRow(header: "Location Data", content: "No data available")
-//                }
             }
         }
     }
