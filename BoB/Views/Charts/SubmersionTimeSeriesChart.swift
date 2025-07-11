bed04b46d591b4bc35089f07d6c76ecebe5635ca
Kim Martini <dr.kim.martini@gmail.com>
2025-07-11 09:33:20 -0700

    Added little info bubble to submersion data

--- BoB/Views/Charts/SubmersionTimeSeriesChart.swift
+++ BoB/Views/Charts/SubmersionTimeSeriesChart.swift
@@ -20,6 +20,12 @@ struct SubmersionTimeSeriesChart: View {
         submersionData.map{ $0.temperature }.max() ?? 120.0
     }
     
+    @State private var showSelectionBar = false
+    @State private var offsetX = 0.0
+    @State private var offsetY = 0.0
+    @State private var selectedTempString: String = ""
+    @State private var selectedDepthString: String = ""
+    
     var body: some View {
         VStack {
             Chart(submersionData) { item in
@@ -39,6 +45,62 @@ struct SubmersionTimeSeriesChart: View {
                 .accessibilityValue("Depth: \(item.depth) meters")
             }
             .chartYAxisLabel("Depth [m]")
+            .chartOverlay {proxy in
+                GeometryReader { geometry in
+                    Rectangle()
+                        .foregroundStyle(.fandango)
+                        .frame(width: 2, height: geometry.size.height * 0.95)
+                        .opacity(showSelectionBar ? 1.0 : 0.0)
+                        .offset(x: offsetX)
+                    
+                    Capsule()
+                        .foregroundStyle(.fandango)
+                        .frame(width: 60, height: 25)
+                        .overlay {
+                            VStack {
+                                Text("\(selectedDepthString) m")
+                                    .font(.caption)
+                            }
+                            .foregroundStyle(.white.gradient)
+                        }
+                        .opacity(showSelectionBar ? 1.0 : 0.0)
+                        .offset(x: offsetX - 30, y: offsetY - 30)
+                    
+                    Rectangle()
+                        .fill(.clear)
+                        .contentShape(Rectangle())
+                        .gesture(
+                            DragGesture()
+                                .onChanged { value in
+                                    if !showSelectionBar {
+                                        showSelectionBar = true
+                                    }
+                                    
+                                    if let plotFrame = proxy.plotFrame {
+                                        let origin = geometry[plotFrame].origin
+                                        let location = CGPoint(
+                                            x: value.location.x - origin.x,
+                                            y: value.location.y - origin.y
+                                        )
+                                        offsetX = location.x
+                                        offsetY = location.y
+                                        
+                                        if let date: Date = proxy.value(atX: location.x) {
+                                            if let selectedRecord: WaterSubmersionData = findNearestData(to: date) {
+                                                selectedDepthString =  String(format: "%.1f", selectedRecord.depth)
+                                                selectedTempString =  String(format: "%.1f", selectedRecord.temperature)
+                                            }
+                                            
+                                        }
+                                    }
+                                }
+                                .onEnded( {_ in
+                                    showSelectionBar = false
+                                })
+                        )
+                }
+            }
+            
             
             Chart(submersionData) { item in
                 LineMark(
@@ -58,8 +120,67 @@ struct SubmersionTimeSeriesChart: View {
             }
             .chartYScale(domain: minTemperature...maxTemperature)
             .chartYAxisLabel("Temperature [°C]")
+            .chartOverlay {proxy in
+                GeometryReader { geometry in
+                    Rectangle()
+                        .foregroundStyle(.fandango)
+                        .frame(width: 2, height: geometry.size.height * 0.95)
+                        .opacity(showSelectionBar ? 1.0 : 0.0)
+                        .offset(x: offsetX)
+                    
+                    Capsule()
+                        .foregroundStyle(.fandango)
+                        .frame(width: 60, height: 25)
+                        .overlay {
+                            VStack {
+                                Text("\(selectedTempString) °C")
+                                    .font(.caption)
+                            }
+                            .foregroundStyle(.white.gradient)
+                        }
+                        .opacity(showSelectionBar ? 1.0 : 0.0)
+                        .offset(x: offsetX - 30, y: offsetY - 30)
+                    
+                    Rectangle()
+                        .fill(.clear)
+                        .contentShape(Rectangle())
+                        .gesture(
+                            DragGesture()
+                                .onChanged { value in
+                                    if !showSelectionBar {
+                                        showSelectionBar = true
+                                    }
+                                    
+                                    if let plotFrame = proxy.plotFrame {
+                                        let origin = geometry[plotFrame].origin
+                                        let location = CGPoint(
+                                            x: value.location.x - origin.x,
+                                            y: value.location.y - origin.y
+                                        )
+                                        offsetX = location.x
+                                        offsetY = location.y
+                                        
+                                        if let date: Date = proxy.value(atX: location.x) {
+                                            if let selectedRecord: WaterSubmersionData = findNearestData(to: date) {
+                                                selectedDepthString =  String(format: "%.1f", selectedRecord.depth)
+                                                selectedTempString =  String(format: "%.1f", selectedRecord.temperature)
+                                            }
+                                            
+                                        }
+                                    }
+                                }
+                                .onEnded( {_ in
+                                    showSelectionBar = false
+                                })
+                        )
+                }
+            }
         }
         .frame(height: 300)
+    }
+    
+    func findNearestData(to date: Date) -> WaterSubmersionData? {
+        submersionData.min(by: { abs( timeStampManager.ISO8601StringtoDate($0.timestamp)!.timeIntervalSince(date)) < abs( timeStampManager.ISO8601StringtoDate($1.timestamp)!.timeIntervalSince(date)) })
     }
 }
 
